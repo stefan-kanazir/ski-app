425bff59fa07de876d152f6a42ee8dc7b0fe30fb
stefan-kanazir <kanazir.stefan@gmail.com>
2024-02-17 20:27:28 +0100

    Move placeholder data to separate file

---
+++ src/data.js
@@ -0,0 +1,34 @@
+// This is temp file for placeholder data
+
+export const INSTRUCTORS = [
+  {
+    name: "John Doe",
+    image_url: "https://images.unsplash.com/photo-1565992441121-4367c2967103",
+  },
+  {
+    name: "John Doe",
+    image_url: "https://images.unsplash.com/photo-1565992441121-4367c2967103",
+  },
+  {
+    name: "John Doe",
+    image_url: "https://images.unsplash.com/photo-1565992441121-4367c2967103",
+  },
+];
+
+export const MOUNTAINS = [
+  {
+    name: "Lorem Ipsum",
+    slug: "lorem-ipsum",
+    image_url: "https://images.unsplash.com/25/peak.jpg",
+  },
+  {
+    name: "Lorem Ipsum",
+    slug: "lorem-ipsum",
+    image_url: "https://images.unsplash.com/25/peak.jpg",
+  },
+  {
+    name: "Lorem Ipsum",
+    slug: "lorem-ipsum",
+    image_url: "https://images.unsplash.com/25/peak.jpg",
+  },
+];
